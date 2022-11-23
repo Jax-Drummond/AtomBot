20c6023791f008f92ed34eef56829d3418f36afa
Jax <jaxdrummond9@gmail.com>
2022-11-23 16:13:22 -0500

    Edit: Changed requirements.txt

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ import disnake as discord
 from config import *
 from disnake.ext import commands
 
-
-
 command_sync_flags = commands.CommandSyncFlags.default()
 command_sync_flags.sync_commands_debug = True
 
@@ -11,22 +9,6 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 intents.reactions = True
-client = discord.Client(intents=intents)
-
-
-
-@client.event
-async def on_ready():
-    print(f'Logged on as {client.user}')
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    print(f'Message from {message.author}: {message.content} in {message.channel.id}')
-
 
 bot = commands.Bot(
     command_prefix='..',
