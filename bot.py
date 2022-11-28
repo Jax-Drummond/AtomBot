a7aff563a4ac12dd051dd5b1d19420d236d7982c
Jax <jaxdrummond9@gmail.com>
2022-11-27 23:01:31 -0500

    Added emoji

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import datetime
 
-import disnake
 import disnake as discord
 
 from config import *
@@ -15,15 +14,18 @@ intents.messages = True
 intents.message_content = True
 intents.reactions = True
 
-activity = disnake.Activity(
-    name="in Horbits Mouth",
-    type=disnake.CustomActivity("Farting"),
+emoji = "<:thisisfine:1037528305202634792>"
+
+custom_activity = discord.CustomActivity(
+    name="Poop",
+    type=discord.ActivityType.custom,
+    emoji=emoji,
 )
 
 bot = commands.InteractionBot(
     command_sync_flags=command_sync_flags,
     intents=intents,
-    activity=activity,
+    activity=custom_activity,
 
 )
 
@@ -35,22 +37,28 @@ async def on_ready():
 
 
 @bot.slash_command(description="Test Command that responds with 'World'")
-async def hello(inter):
-    await inter.response.send_message("World")
+async def ping(inter):
+    await inter.response.send_message("Pong")
 
 
 @bot.slash_command(description="Scrapes a random image from Prnt.sc")
-async def prntsc(inter):
-    embed = disnake.Embed(
+async def prntsc(inter: discord.ApplicationCommandInteraction):
+    embed = discord.Embed(
         title="Print-screen Image",
-        color=disnake.Color.blue(),
+        color=discord.Color.blue(),
         timestamp=datetime.datetime.now(),
     )
     image = get_image()
-    embed.set_image(file=disnake.File(image))
-    print(embed.image)
-    await inter.response.send_message(embed=embed)
+    embed.set_image(file=discord.File(image))
+    await inter.response.send_message(embed=embed,components=[
+        discord.ui.Button(label="Again",style=discord.ButtonStyle.blurple,custom_id="Again")
+    ])
     delete_photos()
 
+    @bot.event
+    async def on_button_click(inter: discord.MessageInteraction):
+        if inter.component.custom_id == "Again":
+            await prntsc(inter)
 
 bot.run(TOKEN)
+
