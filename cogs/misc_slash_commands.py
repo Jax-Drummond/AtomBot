81532d94c57b4fc5a95a0f2541378eb5f3c94839
Jax <jaxdrummond9@gmail.com>
2022-12-17 23:14:40 -0500

    Changes: Removed Test

--- cogs/misc_slash_commands.py
+++ cogs/misc_slash_commands.py
@@ -1,34 +1,7 @@
-import asyncio
-import wave
-
 import disnake as discord
-import pyaudio as pyaudio
 from disnake.ext import commands
 
 from utils.bot_utils import load_cogs
-
-
-async def record_audio(channel, duration):
-    def callback(in_data, frame_count, time_info, status):
-        frames.append(in_data)
-        return (None, pyaudio.paContinue)
-
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16, channels=1, rate=48000, input=True, frames_per_buffer=1024,
-                    stream_callback=callback)
-    frames = []
-    stream.start_stream()
-    await asyncio.sleep(duration)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open("recorded_audio.wav", "wb")
-    wf.setnchannels(1)
-    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    wf.setframerate(48000)
-    wf.writeframes(b"".join(frames))
-    wf.close()
 
 
 class Misc_Slash_Commands(commands.Cog):
@@ -42,22 +15,6 @@ class Misc_Slash_Commands(commands.Cog):
     async def reload(self, inter: discord.ApplicationCommandInteraction):
         load_cogs(self.bot, True)
         await inter.response.send_message("Reloaded cogs.", ephemeral=True, delete_after=5)
-
-    @commands.slash_command(description="Stuff")
-    async def record(self, inter, duration: int | int = 10):
-        channel = inter.author.voice.channel
-        await channel.connect()
-        await record_audio(channel, duration)
-        await channel.disconnect()
-
-    @commands.slash_command(description="Stuff2")
-    async def play(self, inter):
-        channel = inter.author.voice.channel
-        vc = await channel.connect()
-        vc.play(discord.FFmpegPCMAudio("recorded_audio.wav"))
-        vc.source = discord.PCMVolumeTransformer(vc.source)
-        vc.source.volume = 0.5
-        await vc.disconnect()
 
 
 def setup(bot):
