e84a6230263b94e32183963dd71e7e911561b3cf
Jax <jaxdrummond9@gmail.com>
2022-12-20 14:46:37 -0500

    Changes: Added signal server command

--- cogs/misc_slash_commands.py
+++ cogs/misc_slash_commands.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import disnake as discord
 from disnake.ext import commands
 
@@ -17,10 +19,23 @@ class Misc_Slash_Commands(commands.Cog):
         load_cogs(self.bot, True)
         await inter.response.send_message("Reloaded cogs.", ephemeral=True, delete_after=5)
 
-    @commands.slash_command(description="Get status of a server")
+    @commands.slash_command(description="Get the status of a server", name="server-status")
     async def get_server_status(self, inter, server: str = commands.Param(choices=get_servers().keys())):
         servers = get_servers()
-        await inter.response.send_message(f"Status of {server} is {await get_server_status(servers[server])}")
+        await inter.response.send_message(
+            f"The current status of {server} is {await get_server_status(servers[server])}")
+
+    @commands.slash_command(description="Sends server power command", name="signal-server")
+    @commands.default_member_permissions(administrator=True)
+    async def control_server_power(self, inter,
+                                   signal: str = commands.Param(choices=["start", "stop", "restart", "kill"]),
+                                   server: str = commands.Param(choices=get_servers().keys())):
+        servers = get_servers()
+        await change_power_state(servers[server], signal)
+        await inter.response.defer()
+        await asyncio.sleep(3)
+        await inter.followup.send(
+            f"The current status of {server} is {await get_server_status(servers[server])}")
 
 
 def setup(bot):
--- utils/pterodactyl_api.py
+++ utils/pterodactyl_api.py
@@ -8,7 +8,6 @@ headers_client = {
     "Content-Type": "application/json",
     "CF-Access-Client-Id": f"{CF_CLIENT_ID}",
     "CF-Access-Client-Secret": f"{CF_CLIENT_SECRET}",
-    "cookie": "pterodactyl_session=eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%253D%253D"
 }
 
 headers_application = {
@@ -28,9 +27,9 @@ async def get_server_status(server):
     data = response.json()
     state = data['attributes']['current_state']
     if state == "running":
-        return "Online"
+        return "ONLINE"
     else:
-        return "Offline"
+        return state.upper()
 
 
 def get_servers():
@@ -38,6 +37,13 @@ def get_servers():
     data = response.json()
     for f in data['data']:
         server_name = f['attributes']['name']
-        server_uuid = f['attributes']['uuid']
+        server_uuid = f['attributes']['identifier']
         servers.update({server_name: server_uuid})
     return servers
+
+
+async def change_power_state(server, state):
+    payload = '{"signal": "%s"}' % state
+    response = requests.request('POST', f'{BASE_URL}api/client/servers/{server}/power', data=payload,
+                                headers=headers_client)
+    print(response.text)
