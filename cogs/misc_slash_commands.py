0749868554cf6639fb8595d72cf26cac834b2d58
Jax <jaxdrummond9@gmail.com>
2022-12-20 12:53:01 -0500

    Changes: Added Ptero Server Status command

--- cogs/misc_slash_commands.py
+++ cogs/misc_slash_commands.py
@@ -2,6 +2,7 @@ import disnake as discord
 from disnake.ext import commands
 
 from utils.bot_utils import load_cogs
+from utils.pterodactyl_api import *
 
 
 class Misc_Slash_Commands(commands.Cog):
@@ -15,6 +16,11 @@ class Misc_Slash_Commands(commands.Cog):
     async def reload(self, inter: discord.ApplicationCommandInteraction):
         load_cogs(self.bot, True)
         await inter.response.send_message("Reloaded cogs.", ephemeral=True, delete_after=5)
+
+    @commands.slash_command(description="Get status of a server")
+    async def get_server_status(self, inter, server: str = commands.Param(choices=get_servers().keys())):
+        servers = get_servers()
+        await inter.response.send_message(f"Status of {server} is {await get_server_status(servers[server])}")
 
 
 def setup(bot):
---
+++ utils/pterodactyl_api.py
@@ -0,0 +1,43 @@
+import requests
+
+from config import *
+
+headers_client = {
+    "Authorization": f"Bearer {CLIENT_KEY_PTERO}",
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "CF-Access-Client-Id": f"{CF_CLIENT_ID}",
+    "CF-Access-Client-Secret": f"{CF_CLIENT_SECRET}",
+    "cookie": "pterodactyl_session=eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%253D%253D"
+}
+
+headers_application = {
+    "Authorization": f"Bearer {APP_KEY_PTERO}",
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "CF-Access-Client-Id": f"{CF_CLIENT_ID}",
+    "CF-Access-Client-Secret": f"{CF_CLIENT_SECRET}",
+    "cookie": "pterodactyl_session=eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%253D%253D"
+}
+
+servers = {}
+
+
+async def get_server_status(server):
+    response = requests.request('GET', f'{BASE_URL}api/client/servers/{server}/resources', headers=headers_client)
+    data = response.json()
+    state = data['attributes']['current_state']
+    if state == "running":
+        return "Online"
+    else:
+        return "Offline"
+
+
+def get_servers():
+    response = requests.request('GET', f'{BASE_URL}api/application/servers', headers=headers_application)
+    data = response.json()
+    for f in data['data']:
+        server_name = f['attributes']['name']
+        server_uuid = f['attributes']['uuid']
+        servers.update({server_name: server_uuid})
+    return servers
